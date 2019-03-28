1f16ff82395b7969de5ccf16d2b2438b24633f44
Carlos Rogério Bittencourt <bittencourtcrb@gmail.com>
2019-03-28 17:34:53 -0300

    Update crv.sql

--- banco/crv.sql
+++ banco/crv.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 20-Mar-2019 às 01:41
--- Versão do servidor: 10.1.38-MariaDB
--- versão do PHP: 7.3.2
+-- Generation Time: 27-Mar-2019 às 18:52
+-- Versão do servidor: 10.1.37-MariaDB
+-- versão do PHP: 7.3.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -48,6 +48,15 @@ CREATE TABLE `cliente` (
   `nome_fantasia` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Extraindo dados da tabela `cliente`
+--
+
+INSERT INTO `cliente` (`id_cliente`, `nome`, `email`, `logradouro`, `numero`, `bairro`, `cidade`, `estado`, `cep`, `telefone`, `celular`, `cpf`, `rg`, `data_nascimento`, `cnpj`, `inscricao_estadual`, `nome_fantasia`) VALUES
+(1, 'Maria José', 'maria.jose@gmail.com', 'Rua Dirce Migliorini', 326, 'Jardim Nápoli', 'Sorocaba', 'São Paulo', '18071-426', '1525866324', '15964521865', '764.881.100-63', '22.683.978-3', '1985-06-08', NULL, NULL, NULL),
+(2, 'Clube de Futebol São Bento', 'sao.bento@gmail.com', 'Rua Rúbens Cleto', 253, 'Jardim Los Angeles', 'Sorocaba', 'São Paulo', '18074-050', '1563254114', '15987456521', NULL, NULL, NULL, '68.565.744/0001-97', '668.596.650.607', 'São Bento'),
+(3, 'José João', 'jose.joao@gmail.com.br', 'Rua Faustino Rodrigues Martins', 852, 'Vila Mineirão', 'Sorocaba', 'São Paulo', '18076-500', '1574258413', '15965124585', '682.964.310-41', '12.420.268-8', '1980-12-15', NULL, NULL, NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -74,6 +83,15 @@ CREATE TABLE `funcionario` (
   `tipo` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Extraindo dados da tabela `funcionario`
+--
+
+INSERT INTO `funcionario` (`id_funcionario`, `nome`, `data_nascimento`, `cpf`, `rg`, `data_admissao`, `logradouro`, `numero`, `bairro`, `cidade`, `estado`, `cep`, `email`, `telefone`, `celular`, `senha`, `tipo`) VALUES
+(1, 'Victor Ludwick', '1998-10-21', '586.669.830-78', '27.283.244-3', '2018-09-10', 'Estrada Saladino Duarte de Oliveira', 754, 'Caputera', 'Sorocaba', 'São Paulo', '18017-362', 'victor.ludwick@gmail.com', '1532926548', '15981157644', '123', 1),
+(2, 'Rafael Martins', '1999-08-21', '727.388.730-73', '25.154.367-5', '2018-06-12', 'Rua Voluntário Altino', 164, 'Vila Hortência', 'Sorocaba', 'São Paulo', '18020-290', 'rafael.martins@gmail.com', '1546784965', '15985463254', '123', 1),
+(3, 'Carlos Rogério', '1992-02-11', '145.840.200-27', '46.348.918-4', '2017-07-17', 'Rua Mário Jonas', 245, 'Jardim Celeste', 'Sorocaba', 'São Paulo', '18066-075', 'carlos.rogerio@gmail.com', '1564585214', '15986565412', '123', 0);
+
 -- --------------------------------------------------------
 
 --
@@ -87,6 +105,14 @@ CREATE TABLE `nota_fiscal` (
   `id_pedido` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Extraindo dados da tabela `nota_fiscal`
+--
+
+INSERT INTO `nota_fiscal` (`id_nota`, `numero`, `data_emissao`, `id_pedido`) VALUES
+(1, '13468525987463589426', '2019-03-11', 1),
+(2, '85369521647528565235', '2019-03-12', 2);
+
 -- --------------------------------------------------------
 
 --
@@ -98,8 +124,19 @@ CREATE TABLE `pedido` (
   `data_pedido` date NOT NULL,
   `total_pedido` double NOT NULL,
   `id_cliente` int(11) NOT NULL,
-  `id_funcionario` int(11) NOT NULL
+  `id_funcionario` int(11) NOT NULL,
+  `tipo` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `pedido`
+--
+
+INSERT INTO `pedido` (`id_pedido`, `data_pedido`, `total_pedido`, `id_cliente`, `id_funcionario`, `tipo`) VALUES
+(1, '2019-03-11', 243.68, 1, 1, 1),
+(2, '2019-03-12', 236.89, 3, 2, 1),
+(3, '2019-03-20', 333.88, 3, 3, 0),
+(4, '2019-03-15', 179.39, 2, 2, 0);
 
 -- --------------------------------------------------------
 
@@ -113,6 +150,20 @@ CREATE TABLE `pedido_produto` (
   `quantidade` int(11) NOT NULL,
   `valor_total` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `pedido_produto`
+--
+
+INSERT INTO `pedido_produto` (`id_pedido`, `id_produto`, `quantidade`, `valor_total`) VALUES
+(1, 1, 2, 193.98),
+(1, 2, 1, 49.7),
+(2, 1, 1, 96.99),
+(2, 5, 1, 139.9),
+(3, 1, 2, 193.98),
+(3, 5, 1, 139.9),
+(4, 2, 2, 99.4),
+(4, 4, 1, 79.99);
 
 -- --------------------------------------------------------
 
@@ -128,6 +179,17 @@ CREATE TABLE `produto` (
   `codigo_barras` varchar(50) NOT NULL,
   `quantidade` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `produto`
+--
+
+INSERT INTO `produto` (`id_produto`, `nome`, `preco`, `descricao`, `codigo_barras`, `quantidade`) VALUES
+(1, 'Luvas Boxe', 96.99, 'Par de luvas de boxe.', '13467985213265874125', 15),
+(2, 'Luvas Para Goleiro', 49.7, 'Par de luvas de goleiro.', '15478745963265874125', 32),
+(3, 'Bola de Basquete', 76.49, 'Bola de basquete com medidas oficiais.', '36525269854712453698', 12),
+(4, 'Bola de Futebol', 79.99, 'Bola de Futebol com medidas oficiais.', '54147858472369563214', 6),
+(5, 'Saco de Areia', 139.9, 'Saco de areia para pratica de artes marciais.', '21453962795386425963', 26);
 
 --
 -- Indexes for dumped tables
@@ -172,9 +234,7 @@ ALTER TABLE `pedido`
 -- Indexes for table `pedido_produto`
 --
 ALTER TABLE `pedido_produto`
-  ADD PRIMARY KEY (`id_pedido`,`id_produto`) USING BTREE,
-  ADD UNIQUE KEY `id_produto` (`id_produto`),
-  ADD UNIQUE KEY `id_pedido` (`id_pedido`);
+  ADD PRIMARY KEY (`id_pedido`,`id_produto`) USING BTREE;
 
 --
 -- Indexes for table `produto`
@@ -191,31 +251,31 @@ ALTER TABLE `produto`
 -- AUTO_INCREMENT for table `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `funcionario`
 --
 ALTER TABLE `funcionario`
-  MODIFY `id_funcionario` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_funcionario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `nota_fiscal`
 --
 ALTER TABLE `nota_fiscal`
-  MODIFY `id_nota` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_nota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `pedido`
 --
 ALTER TABLE `pedido`
-  MODIFY `id_pedido` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_pedido` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `produto`
 --
 ALTER TABLE `produto`
-  MODIFY `id_produto` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_produto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
