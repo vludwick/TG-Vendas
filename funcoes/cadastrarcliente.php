f330e7799ea23205bce7916fab4c5808d556a8c4
Carlos Rogério Bittencourt <bittencourtcrb@gmail.com>
2019-04-04 16:03:48 -0300

    Update cadastrarcliente.php

--- funcoes/cadastrarcliente.php
+++ funcoes/cadastrarcliente.php
@@ -128,6 +128,9 @@
         $('#cnpj').mask('00.000.000/0000-00');
         $('#celular').mask('(00) 00000-0000');
         $('#telefone').mask('(00) 0000-0000');
+    
+	$(function(){
+
         $('#addcliente').submit(function(event){
 		event.preventDefault();
 		var formDados = new FormData($(this)[0]);
